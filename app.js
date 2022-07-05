ee01b084c25eb8a9ed9d20df4430c2c480a36bc6
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-05 07:20:44 +0100

    add models

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require("mongoose");
+var mongoDB =
+  "mongodb+srv://davisphem:phemmy2022@cluster0.an92mve.mongodb.net/locallibv2?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
