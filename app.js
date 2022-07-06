30fb82e77e140886e74930c372bf8fc64ab62be7
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-06 13:55:01 +0100

    update app.js

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ if (debugit) {
   require("dotenv").config();
 }
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require('./routes/catalog')
+const app = express();
 
 //Set up mongoose connection
-var mongoose = require("mongoose");
-var mongoDB =
+const mongoose = require("mongoose");
+const mongoDB =
   "mongodb+srv://davisphem:phemmy2022@cluster0.an92mve.mongodb.net/locallibv2?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
@@ -34,7 +34,7 @@ app.use("/media", express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use('/catalog', catalogRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
